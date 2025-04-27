c28d695644e3121a0c6b7401f1e7fd422b721bba
shakib-mia <smdshakibmia2001@gmail.com>
2025-04-27 22:10:39 +0600

    Enhance index.html and styles for improved layout; add client logos and configure carousels for better user experience.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,27 @@ $(document).ready(function () {
   $("header .owl-carousel").owlCarousel({
     items: 1,
     loop: true,
-    autoplay: {
-      delay: 1,
-    },
+    autoplay: true, // Enable autoplay
+    autoplayTimeout: 3000, // Delay in milliseconds
     dots: true,
     nav: false,
     animateIn: "animate__fadeInRightBig",
     animateOut: "animate__fadeOutLeftBig",
+  });
+
+  $("#clients .owl-carousel").owlCarousel({
+    items: 7,
+    loop: true,
+    autoplay: true, // Enable autoplay
+    autoplayTimeout: 2000, // Delay in milliseconds
+    dots: false,
+    nav: false,
+    // animateIn: "animate__fadeInRightBig",
+    // animateOut: "animate__fadeOutLeftBig",
+    responsive: {
+      0: { items: 2 },
+      768: { items: 3 },
+      1024: { items: 7 },
+    },
   });
 });
